6430451c6ef3af08165b92bf0469f3e3be9cb873
Nancy Saini <nancysaini568@gmail.com>
2026-08-27 00:37:22 +0530

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const port=process.env.PORT || 5000
+
+const app=express()
+
+app.get("/health",(req,res)=>{
+res.status(200).json({message:"All is well"})
+})
+
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"Hello nancy"})
+})
+
+app.listen(port,()=>{
+    console.log(`server started ${port}`)
+})
